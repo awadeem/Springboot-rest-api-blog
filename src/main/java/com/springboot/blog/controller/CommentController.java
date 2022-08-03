5145f7991a3a97c8f41fb65b5ad7288e7f6a7623
cd-alvina <alvina@codedifferently.com>
2022-08-03 18:11:34 -0400

    add comment APIs

---
+++ src/main/java/com/springboot/blog/controller/CommentController.java
@@ -0,0 +1,65 @@
+package com.springboot.blog.controller;
+
+import com.springboot.blog.model.Comment;
+import com.springboot.blog.payload.CommentDto;
+import com.springboot.blog.payload.PostDto;
+import com.springboot.blog.service.CommentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/")
+public class CommentController {
+
+    private CommentService commentService;
+
+    @Autowired
+    public CommentController(CommentService commentService) {
+        this.commentService = commentService;
+    }
+
+    @PostMapping("/posts/{postId}/comments")
+    public ResponseEntity<CommentDto> createComment(@PathVariable(value = "postId")  Long postId,
+                                                    @RequestBody CommentDto commentDto){
+
+        return new ResponseEntity<>(commentService.commentCreate(postId,commentDto), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/posts/{postId}/comments")
+    public List<CommentDto> getCommentsById(@PathVariable(value = "postId") Long postId){
+        return commentService.getCommentsByPostId(postId);
+
+    }
+
+    @GetMapping("/posts/{postId}/comments/{id}")
+    public ResponseEntity<CommentDto> getCommentById(@PathVariable(value = "postId") Long postId,
+                                                     @PathVariable(value = "id") Long commentId){
+        CommentDto commentDto = commentService.getCommentById(postId, commentId);
+        return new ResponseEntity<>(commentDto, HttpStatus.OK);
+    }
+
+    @PutMapping("/posts/{postId}/comments/{id}")
+    public ResponseEntity<CommentDto> updateComment(@PathVariable(name = "postId") Long postId,
+                                                    @PathVariable(value = "id") Long commentId,
+                                                    /*RequestBody convert json object into java object by using HTTP
+                                                    message converters */
+                                                    @RequestBody CommentDto commentDto){
+
+        CommentDto updatedComment = commentService.updateComment(postId,commentId,commentDto);
+        return new ResponseEntity<>(updatedComment,HttpStatus.OK);
+    }
+
+    @DeleteMapping("/posts/{postId}/comments/{id}")
+    public ResponseEntity<String> deleteComment(@PathVariable(name = "postId") Long postId,
+                                                    @PathVariable(value = "id") Long commentId){
+       commentService.deleteComment(postId,commentId);
+        return new ResponseEntity<>("Comment entity deleted successfully. ", HttpStatus.OK);
+
+    }
+
+
+}
